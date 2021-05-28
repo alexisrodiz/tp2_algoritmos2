38d0f51e40a17ac174621e585ef253a7163df13e
kikiymini <caramayo@fi.uba.ar>
2021-05-28 17:17:50 -0300

    Se agregan pre y post

--- Jugador.h
+++ Jugador.h
@@ -21,12 +21,42 @@ private:
 public:
 	Jugador();
 
+	// Metodos
+	/*
+	 * Pre: indicar el numero del jugador
+	 * Post: establece el numero al jugador correspondiente
+	 */
 	void establecerJugador(unsigned int jugador);
+	/*
+	 * Pre:
+	 * Post: Se establece la cantidad de fichas en total por jugador
+	 */
 	void establecerFichasTotal();
+	/*
+	 * Pre: existir jugadores
+	 * Post: Se asigna la ficha
+	 */
 	void ingresarFicha();
+	/*
+	 * Pre: existir un jugador
+	 * Post: se devuelve el numero del jugador correspondiente
+	 */
 	unsigned int obtenerJugador();
+	/*
+	 * Pre: establecer las fichas totales
+	 * Post: Devolver las fichas totales
+	 */
 	unsigned int obtenerFichasTotal();
+	/*
+	 * Pre: Establecer ficha al jugador
+	 * Post: Devuelve la ficha correspondiente al jugador indicado
+	 */
 	std::string obtenerFicha();
+	/*
+	 * Pre: Asignación de ficha
+	 * Post: Controla que la ficha del 2do jugador no sea igual
+	 * 		a la del primero
+	 */
 	bool controlFicha(std::string ficha[2]);
 	void imprimirJugador();
 };
