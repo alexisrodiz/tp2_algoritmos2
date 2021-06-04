fd96e6a8248def463c0ed638048f0fb8700207c3
carlos <carlosorqueda@gmail.com>
2021-06-03 21:02:59 -0300

    Celda.h actualizado, se agrego propiedades y definiciones de métodos

--- Celda.h
+++ Celda.h
@@ -5,9 +5,35 @@ class Celda {
 
     private:
 
-        
+        std::string valorDeLaCelda;
+        std::string estadoDeLaCelda;
 
+        /* Posibles valores de:
+        *  valorDeLaCelda = " " | Jugador->marca
+        *  estadoDeLaCelda = "vacia" | "llena"
+        */
+        
     public:
+
+        /* Pre: 
+        *  Pos: Crea una Celda con valorDeLaCelda = '' y estadoDeLaCelda = 'vacia'
+        */
+        Celda();
+
+        /* Pre: 
+        *  Pos: Retorna el valor de la Celda
+        */
+        std::string obtenerValorDeCelda();
+
+        /* Pre: 
+        *  Pos: Retorna el estado de la Celda
+        */
+        std::string obtenerEstadoDeCelda();
+
+        /* Pre: 
+        *  Pos: Destruye la Celda
+        */
+        ~Celda();
 
 };
 
